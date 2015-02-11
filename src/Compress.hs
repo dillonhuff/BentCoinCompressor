0e1fd8ecbc68e9e4579d464c7f985173c169e771
Dillon Huff <dillonhuff@gmail.com>
2015-02-11 17:17:28 -0600

    recOneBitLocations now counts length with Word32 as well

--- src/Compress.hs
+++ src/Compress.hs
@@ -6,12 +6,15 @@ import Data.ByteString as ByteStr
 
 oneBit = BitStr.take 1 $ bitString $ singleton 1
 
-oneBitLocations :: BitString -> [Word32]
-oneBitLocations bitStr = recOneBitLocations 0 bitStr
+oneBitLocations :: BitString -> (Word32, [Word32])
+oneBitLocations bitStr = recOneBitLocations 0 0 bitStr
 
-recOneBitLocations :: Word32 -> BitString -> [Word32]
-recOneBitLocations index bitStr = case BitStr.length bitStr == 0 of
-  True -> []
+recOneBitLocations :: Word32 -> Word32 -> BitString -> (Word32, [Word32])
+recOneBitLocations numOnes index bitStr = case BitStr.length bitStr == 0 of
+  True -> (0, [])
   False -> case (BitStr.take 1 bitStr) == oneBit of
-    True -> index : (recOneBitLocations (index + 1) $ BitStr.drop 1 bitStr)
-    False -> recOneBitLocations (index + 1) $ BitStr.drop 1 bitStr
+    True -> ((fst res2) + 1, index : (snd res2))
+    False -> res1
+    where
+      res1 = recOneBitLocations numOnes (index + 1) $ BitStr.drop 1 bitStr
+      res2 = recOneBitLocations (numOnes + 1) (index + 1) $ BitStr.drop 1 bitStr
