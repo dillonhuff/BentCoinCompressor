2c4beec95a6ad6fed401cf2208820e95382d0d5d
Dillon Huff <dillonhuff@gmail.com>
2015-02-11 17:43:38 -0600

    bit truncation for words appears to work

--- src/Compress.hs
+++ src/Compress.hs
@@ -1,10 +1,13 @@
-module Compress(oneBitLocations) where
+module Compress(oneBitLocations,
+                truncateTo14Bits) where
 
-import Data.Word
+import Data.Bits
 import Data.BitString as BitStr
 import Data.ByteString as ByteStr
+import Data.Word
 
 oneBit = BitStr.take 1 $ bitString $ singleton 1
+zeroBit = BitStr.take 1 $ bitString $ singleton 0
 
 oneBitLocations :: BitString -> (Word32, [Word32])
 oneBitLocations bitStr = recOneBitLocations 0 0 bitStr
@@ -18,3 +21,17 @@ recOneBitLocations numOnes index bitStr = case BitStr.length bitStr == 0 of
     where
       res1 = recOneBitLocations numOnes (index + 1) $ BitStr.drop 1 bitStr
       res2 = recOneBitLocations (numOnes + 1) (index + 1) $ BitStr.drop 1 bitStr
+
+truncateTo14Bits :: Word32 -> Maybe BitString
+truncateTo14Bits word = case word >= 16384 of
+  True -> Nothing
+  False -> Just $ truncateBits 14 word
+
+truncateBits :: Int -> Word32 -> BitString
+truncateBits 0 word = BitStr.empty
+truncateBits n word = BitStr.append nextBit restOfBits
+  where
+  nextBit = case word .&. 1 == 1 of
+    True -> oneBit
+    False -> zeroBit
+  restOfBits = truncateBits (n - 1) (shiftR word 1)
--- test/CompressTests.hs
+++ test/CompressTests.hs
@@ -1,6 +1,6 @@
 module CompressTests() where
 
-import Data.BitString
+import Data.BitString as BitStr
 import Data.ByteString
 
 import Compress
@@ -8,6 +8,7 @@ import TestUtils
 
 allTests = do
   oneBitLocationsTests
+  truncateTo14BitsTests
 
 oneBitLocationsTests =
   testFunction oneBitLocations oneBitLocationsTestCases
@@ -18,3 +19,14 @@ oneBitLocationsTestCases =
    ([0], (0, [])),
    ([255], (8, [0, 1, 2, 3, 4, 5, 6, 7])),
    ([32, 1, 1, 3], (5, [5, 8, 16, 24, 25]))]
+
+truncateTo14BitsTests =
+  testFunction truncateTo14Bits truncateTo14BitsTestCases
+
+
+truncateTo14BitsTestCases =
+  [(16384, Nothing),
+   (0, Just $ BitStr.take 14 $ bitString $ pack [0, 0]),
+   (1, Just $ BitStr.take 14 $ bitString $ pack [1, 0]),
+   (255, Just $ BitStr.take 14 $ bitString $ pack [255, 0]),
+   (1024, Just $ BitStr.take 14 $ bitString $ pack [0, 4])]
