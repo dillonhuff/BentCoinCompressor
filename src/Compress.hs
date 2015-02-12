2461028f1749a09024cfbe817d85ca741256e1ba
Dillon Huff <dillonhuff@gmail.com>
2015-02-12 01:45:57 -0600

    deleted old commented out code in Compress

--- src/Compress.hs
+++ src/Compress.hs
@@ -21,19 +21,6 @@ recOneBitLocations index bitStr = case BitStr.length bitStr == 0 of
     where
       rest = recOneBitLocations (index + 1) $ BitStr.drop 1 bitStr
 
-{-oneBitLocations :: BitString -> (Word32, [Word32])
-oneBitLocations bitStr = recOneBitLocations 0 0 bitStr
-
-recOneBitLocations :: Word32 -> Word32 -> BitString -> (Word32, [Word32])
-recOneBitLocations numOnes index bitStr = case BitStr.length bitStr == 0 of
-  True -> (0, [])
-  False -> case (BitStr.take 1 bitStr) == oneBit of
-    True -> ((fst res2) + 1, index : (snd res2))
-    False -> res1
-    where
-      res1 = recOneBitLocations numOnes (index + 1) $ BitStr.drop 1 bitStr
-      res2 = recOneBitLocations (numOnes + 1) (index + 1) $ BitStr.drop 1 bitStr-}
-
 truncateTo14Bits :: Word32 -> BitString
 truncateTo14Bits word = truncateBits 14 word
 
